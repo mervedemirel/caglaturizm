9a2e20e44501668cbefbb11ce00713349283a1fa
samet yilmaz <iamsametyilmaz@gmail.com>
2019-10-31 22:39:24 +0300

    compression

--- server.ts
+++ server.ts
@@ -19,9 +19,11 @@ import 'zone.js/dist/zone-node';
 
 import * as express from 'express';
 import {join} from 'path';
+import * as compression from 'compression';
 
 // Express server
 const app = express();
+app.use(compression);
 
 const PORT = process.env.PORT || 4000;
 const DIST_FOLDER = join(process.cwd(), 'dist/browser');
